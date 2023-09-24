7c0bf00e0f88c1c99b5f7b2dfc82373e60e2257d
Shariar Islam <shariarn85@gmail.com>
2023-09-24 22:33:23 +0600

    banner section added with bg images and overlay effect

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -1,9 +1,40 @@
 
+import background from '../../../assets/images/Rectangle4288.png'
+
+
+const styles = {
+    header: {
+      backgroundImage: `url(${background})`,
+      height: '100vh',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    },
+  
+    content: {
+      height: '100%',
+      width: '100%',
+      backgroundColor: 'rgba(255,255,255, 0.8)',
+    }
+}
+  
+
 const Banner = () => {
     return (
-        <div>
+        <div style={styles.header}>
+            <div style={styles.content}>
             banner
+            </div>
         </div>
+    
+    
+        // <div className=' h-[60vh]' style={{
+        //     backgroundImage: `url(${background})`,
+        //     backgroundRepeat: "no-repeat",
+        //     backgroundSize: "cover"
+        // }}>
+        //     banner
+        // </div> 
     );
 };
 
